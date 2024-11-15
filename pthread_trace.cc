fd50a5b58612dc165506e3ed24c8bc1bed2064cf
Dillon <dsharlet@gmail.com>
2024-11-15 01:43:56 -0800

    Clean up headers

--- pthread_trace.cc
+++ pthread_trace.cc
@@ -7,7 +7,6 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <sched.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
@@ -20,7 +19,6 @@
 #include <cstdlib>
 #include <cstring>
 #include <ctime>
-#include <iostream>
 #include <map>
 #include <mutex>
 #include <thread>
